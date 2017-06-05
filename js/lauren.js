1825dadf257e96e90d2c51854bd5efbd29e7e8aa
Lauren McCarthy <laurmccarthy@gmail.com>
2017-06-05 14:21:18 -0700

    update

---
+++ js/lauren.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+  startPassthrough();
+  document.querySelector('#image-360').setAttribute('material', 'src', '#passthroughVideo');
+
+});
--- js/site.js
+++ js/site.js
@@ -1,16 +1,3 @@
-var phrase = 'Hi, I\'m Lauren and I want to be your smart home.';
-
-function nextChar(i){
-  $('#greeting').text(phrase.substring(0, i));
-  if(i < phrase.length){
-    setTimeout('nextChar(' + (i + 1) + ')', 100);   
-  }
-}
-
-function exitIntro() {
-  $('#intro').slideToggle(500);
-  blink(true);
-}
 
 var timeout, interval;
 function blink(val) {
@@ -47,8 +34,6 @@ function startRecording() {
 
 $(document).ready(function() {
   startPassthrough();
-  nextChar(0);
-  setTimeout(exitIntro, phrase.length*100+1500);
 
   $('#lauren').mouseover(function() {
     $('#lauren').css('font-weight', '800');

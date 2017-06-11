8a88cee16a45b5e5fb36714cc2d58aff5ba5614b
Lauren McCarthy <laurmccarthy@gmail.com>
2017-06-11 16:36:06 -0700

    styling

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,13 @@ AFRAME.registerComponent('setup', {
     document.querySelector('#image-360').emit('startRotateSky');
     var links = document.querySelector('#links');
     links.addEventListener('animationcomplete', function () {
-      var rot = links.getAttribute('rotation').y + 72;
+      var mult = Math.random() > 0.5 ? 1 : -1;
+      var n = (Math.floor(4 * Math.random()) + 2);
+      var angle = 40 * mult * n;
+      var rot = links.getAttribute('rotation').y + angle;
       links.setAttribute('animation', 'to', '0 '+rot+' 0');
-      setTimeout(function() { links.emit('startRotateHomes'); }, 3000);
+      links.setAttribute('animation', 'dur', 250*n);
+      setTimeout(function() { links.emit('startRotateHomes'); }, 3000 + 250*n);
     });
     links.emit('startRotateHomes');
   }
